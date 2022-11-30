9bd01a81b08d41c49c0bf55da7f4d1adcc45ebf8
Randall Fulton <rfulton@rigetti.com>
2022-11-30 14:42:55 -0600

    neovim: Test output keybind

--- nvim/lua/config/neotest.lua
+++ nvim/lua/config/neotest.lua
@@ -1,28 +1,29 @@
-local neotest = require('neotest')
-local wk = require('which-key')
+local neotest = require("neotest")
+local wk = require("which-key")
 
 neotest.setup({
 	adapters = {
-		require('neotest-go')({
-			args = { '-count=1' },
+		require("neotest-go")({
+			args = { "-count=1" },
 		}),
-		require('neotest-python'),
-		require('neotest-rust'),
-	}
+		require("neotest-python"),
+		require("neotest-rust"),
+	},
 })
 
 local maps = {
-	['<leader>'] = {
+	["<leader>"] = {
 		i = {
-			t = { '<cmd>lua require("neotest").summary.toggle()<CR>', 'Tests' },
+			t = { '<cmd>lua require("neotest").summary.toggle()<CR>', "Tests" },
 		},
 		t = {
-			name = 'Tests',
-			d = { '<cmd>lua require("neotest").run.run({ strategy = "dap" })<CR>', 'Debug nearest' },
-			f = { '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', 'File' },
-			s = { '<cmd>lua require("neotest").run.stop()<CR>', 'Stop nearest' },
-			t = { '<cmd>lua require("neotest").run.run()<CR>', 'Nearest' },
-		}
-	}
+			name = "Tests",
+			d = { '<cmd>lua require("neotest").run.run({ strategy = "dap" })<CR>', "Debug nearest" },
+			f = { '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', "File" },
+			o = { '<cmd>lua require("neotest").output.open()<CR>', "Show output" },
+			s = { '<cmd>lua require("neotest").run.stop()<CR>', "Stop nearest" },
+			t = { '<cmd>lua require("neotest").run.run()<CR>', "Nearest" },
+		},
+	},
 }
 wk.register(maps, { silent = true, noremap = true })
