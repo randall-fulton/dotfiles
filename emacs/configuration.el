ae53edc9b04873409617b5d3c56ed9d65bd7f86b
Randall Fulton <randall.ml.fulton@gmail.com>
2023-02-18 08:04:48 -0600

    emacs: haskell indentation

--- emacs/configuration.el
+++ emacs/configuration.el
@@ -3,6 +3,8 @@
 (setq straight-vc-git-default-protocol 'ssh)
 (setq straight-vc-git-force-protocol t)
 
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (menu-bar-mode -1)
@@ -10,7 +12,7 @@
 (tool-bar-mode -1)
 
 (setq default-directory
-	      (cond ((equal system-type 'windows-nt) "G:\\Developer")
+	  (cond ((equal system-type 'windows-nt) "G:\\Developer")
 			(t "~/dev")))
 
 (setq inhibit-startup-screen t)
@@ -19,39 +21,39 @@
 (setq-default tab-width 4)
 
 (set-frame-font (cond ((eq system-type 'darwin) "FiraMono Nerd Font Mono 16")
-					      ((eq system-type 'gnu/linux) "Iosevka 12")
-					      ((eq system-type 'windows-nt) "Fira Code 12"))
+					  ((eq system-type 'gnu/linux) "Iosevka 12")
+					  ((eq system-type 'windows-nt) "Fira Code 12"))
 				nil t)
 
 (use-package gruvbox-theme
-      :config
-      (load-theme 'gruvbox))
+  :config
+  (load-theme 'gruvbox))
 
 (use-package ligature
-      :load-path "path-to-ligature-repo"
-      :config
-      ;; Enable the "www" ligature in every possible major mode
-      (ligature-set-ligatures 't '("www"))
-      ;; Enable traditional ligature support in eww-mode, if the
-      ;; `variable-pitch' face supports it
-      (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-      ;; Enable all Cascadia Code ligatures in programming modes
-      (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
-									       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
-									       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
-									       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
-									       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
-									       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
-									       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
-									       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-									       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
-									       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
-									       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
-									       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
-									       "\\\\" "://"))
-      ;; Enables ligature checks globally in all buffers. You can also do it
-      ;; per mode with `ligature-mode'.
-      (global-ligature-mode t))
+  :load-path "path-to-ligature-repo"
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+									   ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+									   "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+									   "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+									   "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+									   "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+									   "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+									   "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+									   ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+									   "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+									   "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+									   "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+									   "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
@@ -59,9 +61,9 @@
 (setq-default explicit-shell-file-name "/usr/bin/zsh")
 
 (use-package exec-path-from-shell
-      :ensure t
-      :config
-      (when (memq window-system '(mac ns x))
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
 	(exec-path-from-shell-initialize)))
 
 (global-unset-key (kbd "C-x o"))
@@ -71,47 +73,47 @@
 				(lambda () (interactive) (other-window 1)))
 
 (defun rf/config-edit ()
-      "Open configuration file"
-      (interactive)
-      (find-file (expand-file-name "configuration.org" user-emacs-directory)))
+  "Open configuration file"
+  (interactive)
+  (find-file (expand-file-name "configuration.org" user-emacs-directory)))
 (global-set-key (kbd "C-c c") '("config-edit" . rf/config-edit))
 
 (use-package magit
-      :ensure t)
+  :ensure t)
 (use-package which-key
-      :ensure t
-      :config
-      (which-key-mode))
+  :ensure t
+  :config
+  (which-key-mode))
 
 (use-package ivy
-      :ensure t
-      :config
-      (ivy-mode))
+  :ensure t
+  :config
+  (ivy-mode))
 
 (use-package company)
 
 (use-package lsp-ui
-      :bind (("C-c o" . lsp-ui-imenu))
-      :custom
-      (lsp-ui-imenu-window-fix-width t)
-      (lsp-ui-doc-enable t)
-      (lsp-ui-doc-show-with-cursor t)
-      (lsp-ui-doc-delay 1)
-      (lsp-ui-doc-position 'top))
+  :bind (("C-c o" . lsp-ui-imenu))
+  :custom
+  (lsp-ui-imenu-window-fix-width t)
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-show-with-cursor t)
+  (lsp-ui-doc-delay 1)
+  (lsp-ui-doc-position 'top))
 
 (use-package lsp-mode
-      :init
-      (setq lsp-keymap-prefix "C-c l")
-      :commands lsp
-      :custom
-      (lsp-rust-analyzer-cargo-watch-command "clippy"))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :commands lsp
+  :custom
+  (lsp-rust-analyzer-cargo-watch-command "clippy"))
 ;; (use-package yasnippet)
 
 (use-package projectile
-      :bind ("M-p" . projectile-command-map)
-      :init
-      (setq projectile-project-search-path (list default-directory))
-      (projectile-mode +1))
+  :bind ("M-p" . projectile-command-map)
+  :init
+  (setq projectile-project-search-path (list default-directory))
+  (projectile-mode +1))
 
 (use-package flycheck)
 
@@ -126,17 +128,18 @@
 	(add-hook 'before-save-hook #'lsp-organize-imports))
 
 (use-package haskell-mode)
+(use-package hindent)
 
 (use-package parinfer-rust-mode
-      :hook (emacs-lisp-mode lisp-mode)
-      :init
-      (setq parinfer-rust-auto-download t))
+  :hook (emacs-lisp-mode lisp-mode)
+  :init
+  (setq parinfer-rust-auto-download t))
 (use-package slime
-      :init
-      (setq inferior-lisp-program "sbcl --dynamic-space-size 4096")
-      (setq browse-url-handlers
+  :init
+  (setq inferior-lisp-program "sbcl --dynamic-space-size 4096")
+  (setq browse-url-handlers
 		'(("hyperspec" . eww-browse-url)
-		      ("." . browse-url-default-browser))))
+		  ("." . browse-url-default-browser))))
 
 (use-package lsp-pyright
   :hook (python-mode . (lambda ()
@@ -151,12 +154,12 @@
   :straight (odin-mode :type git :host github :repo "randall-fulton/odin-mode"))
 
 (use-package rustic
-      :hook (lsp-deferred yas-minor-mode) ; lsp-rust-analyzer-inlay-hints-mode
-      :init
-      ;; (setq lsp-rust-analyzer-server-display-inlay-hints t)
-      :config
-      (add-hook 'before-save-hook #'lsp-format-buffer)
-      (add-hook 'before-save-hook #'lsp-organize-imports)
-      (push 'rustic-clippy flycheck-checkers))
+  :hook (lsp-deferred yas-minor-mode) ; lsp-rust-analyzer-inlay-hints-mode
+  :init
+  ;; (setq lsp-rust-analyzer-server-display-inlay-hints t)
+  :config
+  (add-hook 'before-save-hook #'lsp-format-buffer)
+  (add-hook 'before-save-hook #'lsp-organize-imports)
+  (push 'rustic-clippy flycheck-checkers))
 
 (use-package yaml-mode)
