621baa7270ca63ee791aa8e37f8d1b341b645771
Randall Fulton <randall.ml.fulton@gmail.com>
2023-02-19 19:04:18 -0600

    emacs: theme org

--- emacs/configuration.el
+++ emacs/configuration.el
@@ -3,7 +3,50 @@
 (setq straight-vc-git-default-protocol 'ssh)
 (setq straight-vc-git-force-protocol t)
 
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(use-package org
+  :config
+  (variable-pitch-mode)
+  (visual-line-mode)
+  (setq display-line-numbers nil
+        org-pretty-entities t
+        org-hide-leading-star t
+        org-hide-emphasis-markers t
+        org-startup-indented t)
+  (let* ((variable-tuple
+          (cond ((x-list-fonts "ETBembo")      '(:font "ETBembo"))
+                ((x-family-fonts "Sans Serif") '(:family "Sans Serif"))
+                (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+         (base-font-color     (face-foreground 'default nil 'default))
+         (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+    (custom-theme-set-faces
+     'user
+     `(variable-pitch ((t (,@variable-tuple :height 180 :weight thin))))
+     '(fixed-pitch ((t (:font "Fira Code" :height 120))))
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))
+     '(org-code ((t (:inherit (shadow fixed-pitch)))))
+     '(org-block ((t (:inherit fixed-pitch))))
+     '(org-code ((t (:inherit (shadow fixed-pitch)))))
+     '(org-document-info ((t (:foreground "dark orange"))))
+     '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+     '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+     '(org-link ((t (:foreground "royal blue" :underline t))))
+     '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+     '(org-property-value ((t (:inherit fixed-pitch))) t)
+     '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+     '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+     '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+     '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))))
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
@@ -139,7 +182,7 @@
   (use-package hindent))
 
 (use-package parinfer-rust-mode
-  :hook (emacs-lisp-mode lisp-mode)
+  :hook (elisp-mode lisp-mode)
   :init
   (setq parinfer-rust-auto-download t))
 (use-package slime
--- emacs/init.el
+++ emacs/init.el
@@ -17,9 +17,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e45f6f1a61b7eb23a400b5a184e1adc87b35ff3db6c668d953655828e30de8a5" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" default))
- '(package-selected-packages))
-(custom-set-faces)
+   '("3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "e45f6f1a61b7eb23a400b5a184e1adc87b35ff3db6c668d953655828e30de8a5" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" default))
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
