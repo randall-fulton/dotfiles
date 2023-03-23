8985f029f7832cc81483a955f16f8628dbee1b80
Randall Fulton <randall.ml.fulton@gmail.com>
2023-03-23 18:54:16 -0500

    emacs: compile config if missing

--- emacs/init.el
+++ emacs/init.el
@@ -11,75 +11,16 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("b0ad9a942146421ebc5c38bdc607ae587f1a7aa9c81d737a61f9cb7d145551ce" "dde643b0efb339c0de5645a2bc2e8b4176976d5298065b8e6ca45bc4ddf188b7" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "e45f6f1a61b7eb23a400b5a184e1adc87b35ff3db6c668d953655828e30de8a5" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" default))
- '(package-selected-packages nil)
- '(safe-local-variable-values
-   '((lsp-rust-features .
-						["parity"])
-	 (lsp-gopls-build-flags .
-							["-tags=unit,integration"])
-	 (projectile-project-test-cmd . "CONTROLLER_LOG_LEVEL=error go test -tags=unit ./...")
-	 (projectile-project-test-cmd . "go test -tags=unit ./...")
-	 (lsp-go-env quote
-				 ((GOFLAGS . "-tags=unit,integration")))
-	 (projectile-project-compilation-cmd . "go vet -tags=unit,integration ./...")
-	 (lsp-go-env quote
-				 ((GOFLAGS . "-tags=unit")))
-	 (lsp-pyright-venv-path . "./venv")
-	 (flycheck-golangci-lint-config . "~/dev/controller/.golangci.yml"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:font "Fira Code 12" :height 120))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#3c3836" :height 2.0 :underline nil))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#3c3836" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#3c3836" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#3c3836" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#3c3836" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#3c3836"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#3c3836"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#3c3836"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#3c3836"))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(variable-pitch ((t (:height 180 :weight thin)))))
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-
-;; refresh config from git
-(defun rf/config-update-from-git ()
-  "Pull latest config"
-  (interactive)
-  (when (require 'magit nil 'noerror)
-    (let ((origin (pwd)))
-      (cd (cond ((equal window-system 'w32) "g:/Developer/dotfiles")
-                (t "~/dev/dotfiles")))
-      (magit-pull-from-upstream nil)
-      (cd origin))))
-
-;; (rf/config-update-from-git)
-
 (straight-use-package 'org)
-(load-file (expand-file-name "configuration.el" user-emacs-directory))
 
+(defun rf/load-org-config (file)
+  "Load configuration in Org file FILE. Tangle first, if needed."
+  (let ((config-src  (expand-file-name file user-emacs-directory))
+	(config-dest (expand-file-name (file-name-with-extension file "el")
+				       user-emacs-directory)))
+    (when (not (file-exists-p config-dest))
+      (require 'ob-tangle)
+      (org-babel-tangle-file config-src config-dest))
+    (load-file config-dest)))
 
+(rf/load-org-config "configuration.org")
