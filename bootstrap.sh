48b43fb05fb3442b7816bd762a4e21e189ced534
Randall Fulton <randall.ml.fulton@gmail.com>
2023-03-23 21:34:46 -0500

    nix: README and multi-system bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,13 @@ msg() {
 }
 
 install_nix() {
+    if command -v nix >/dev/null; then
+	msg "nix already installed"
+	return
+    fi
+    
+    msg "installing nix..."
+
     curl -s -L https://nixos.org/nix/install > /tmp/install-nix.sh
     case "$(uname -s)" in
 	"Darwin")
@@ -15,39 +22,33 @@ install_nix() {
 	"Linux")
 	    sh /tmp/install-nix.sh --daemon
 	    ;;
-	*)
-	    echo "OS '$(uname -s)' not supported by $0"
     esac
 }
 
 install_nix_darwin() {
+    if command -v darwin-rebuild >/dev/null; then
+	msg "nix-darwin already installed"
+	return
+    fi
+    
+    msg "installing nix-darwin..."
     nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
     ./result/bin/darwin-installer    
+    ln -s $(pwd)/darwin-configuration.nix ~/.nixpkgs/darwin-configuration.nix
 }
 
 install_home_manager() {
+    if command -v home-manager >/dev/null; then
+	msg "home-manager already installed"
+    fi
+    
+    msg "installing home-manager..."
     nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
     nix-channel --update
 }
 
-if ! command -v nix >/dev/null; then
-    msg "installing nix..."
-    install_nix
-else
-    msg "nix already installed"
-fi
-
-if ! command -v darwin-rebuild >/dev/null; then
-    msg "installing nix-darwin..."
+install_nix
+if [ "$(uname -s)" -eq "Darwin" ]; then
     install_nix_darwin
-    ln -s $(pwd)/darwin-configuration.nix ~/.nixpkgs/darwin-configuration.nix
-else
-    msg "nix-darwin already installed"
 fi
-
-if ! command -v home-manager >/dev/null; then
-    msg "installing home-manager..."
-    install_home_manager
-else
-    msg "home-manager already installed"
-fi
+install_home_manager
