1129a042a2dbfd8bb54eb030539ced38442eefc2
Randall Fulton <randall@shipt.com>
2023-08-07 11:27:50 -0500

    emacs: Move machine-specific config to custom.el

--- emacs/init.el
+++ emacs/init.el
@@ -11,6 +11,29 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d395c1793e0d64797d711c870571a0033174ca321ed48444efbe640bf692bf4f" "11873c4fbf465b956889adfa9182495db3bf214d9a70c0f858f07f6cc91cbd47" default))
+ '(display-buffer-alist
+   '(("\\\\*compilation\\\\*" display-buffer-in-side-window
+      (side . bottom))
+     ("\\\\*eshell\\\\*" display-buffer-in-side-window
+      (side . bottom))
+     ("\\\\*vterm\\\\*" display-buffer-in-side-window
+      (side . bottom)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:font "Fira Code 16" :height 180))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch))))))
+
 (straight-use-package 'org)
 
 (defun rf/load-org-config (file)
@@ -22,5 +45,9 @@
       (require 'ob-tangle)
       (org-babel-tangle-file config-src config-dest))
     (load-file config-dest)))
+
+;; All machine-specific config should be stored in custom.el
+(when (file-exists-p "custom.el")
+  (load-file "custom.el"))
 
 (rf/load-org-config "configuration.org")
