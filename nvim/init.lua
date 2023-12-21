81877f1e6fad13db7c4a4b4ad67905b4e03cc3ef
Randall Fulton <randall.ml.fulton@gmail.com>
2023-12-21 10:11:25 -0600

    Neovim telescope result tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -315,6 +315,12 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
+    file_ignore_patterns = {
+      ".git/",
+      ".github/",
+      ".idea/",
+      "vendor/",
+    },
   },
 }
 
@@ -333,7 +339,11 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf',
+  function()
+    require('telescope.builtin').find_files({ hidden = true })
+  end,
+  { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
