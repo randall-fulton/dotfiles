863e809014ba15b4a8d8f7c2492abf92d61dbb84
Randall Fulton <randall.ml.fulton@gmail.com>
2024-03-05 09:08:41 -0600

    nvim: ruby

--- nvim/init.lua
+++ nvim/init.lua
@@ -401,7 +401,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'go', 'lua', 'python', 'rust', 'zig', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'go', 'lua', 'python', 'rust', 'ruby', 'zig', 'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
