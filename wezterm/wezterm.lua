abfff796cf27a18ffc21dea587cf356df9a9e142
Randall Fulton <randall.ml.fulton@gmail.com>
2024-08-17 13:11:23 -0500

    Windows-specific WezTerm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,30 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+local is_windows = function()
+    return wezterm.target_triple:find("windows") ~= nil
+end
+
+local is_macos = function()
+    return wezterm.target_triple:find("darwin") ~= nil
+end
+
+local is_linux = function()
+    return wezterm.target_triple:find("linux") ~= nil
+end
+
 config.color_scheme = "Everforest Dark Hard (Gogh)"
-config.font = wezterm.font "Fira Code"
+config.font = wezterm.font_with_fallback({
+    "FiraCode Nerd Font Mono", -- Windows
+    "Fira Code",               -- macOS
+})
 config.font_size = 18
 config.window_decorations = "RESIZE"
+
+if is_windows() then
+    config.default_prog = {
+        "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+    }
+end
 
 return config
